20b7a8ea2b498812f1937b75315339f5223d6410
Crowdbotics <team@crowdbotics.com>
2023-10-03 16:49:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-frost-43735",
+  url: "https://silent-frost-43735.botics.co",
+  api: "https://silent-frost-43735.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
